b76b4bcc42b0776553a53a1af02001d73e59e205
leandroalvesmachado <leandroalvesmachado@gmail.com>
2023-07-03 16:54:07 -0300

    salvando

--- 11-conceitos-fundamentais-para-react/dio/src/App.js
+++ 11-conceitos-fundamentais-para-react/dio/src/App.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [usuario, setUsuario] = useState("");
+  const [count, setCount] = useState("");
+  const [usuarios, setUsuarios] = useState(["Pablo", "José", "Manoel"]);
+
+  const handleAddUser = () => {
+    setUsuarios([...usuarios, usuario]);
+    setUsuario("");
+  }
+
+  useEffect(() => {
+
+  },[]);
 
   return (
     <div className="App">
       <h1>Hello DIO!</h1>
-      <p>1 + 1 = {1 + 1}</p>
-      {arr.map((item) => (
-        <p>{item} * 5 = {item * 5}</p>
+      <div>
+        <input 
+          value={usuario}
+          onChange={(event) => setUsuario(event.target.value)}
+        />
+        <button onClick={handleAddUser}>Adicionar</button>
+      </div>
+      <hr />
+      {usuarios.map((item) => (
+        <p>{item}</p>
       ))}
     </div>
   )
--- 11-conceitos-fundamentais-para-react/dio/src/AppClass.js
+++ 11-conceitos-fundamentais-para-react/dio/src/AppClass.js
@@ -1,21 +1,43 @@
 // exemplo de componente de classe no react (antigo)
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 
-class App extends Component {
-  state = {
-    usuarios: ["Pablo", "José", "Manoel"]
-  };
+// class App extends Component {
+//   state = {
+//     usuarios: ["Pablo", "José", "Manoel"]
+//   };
 
-  render() {
-    const { usuarios } = this.state;
+//   render() {
+//     const { usuarios } = this.state;
 
-    return (
-      <div className="App">
-        <h1>Hello DIO!</h1>
-        {usuarios.map((item) => (
-          <p>{item}</p>
-        ))}
-      </div>
-    );
-  }
+//     return (
+//       <div className="App">
+//         <h1>Hello DIO!</h1>
+//         {usuarios.map((item) => (
+//           <p>{item}</p>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// convertendo componente acima de classe para funcional
+const App = () => {
+  const [usuarios, setUsuarios] = useState(
+    ["Pablo", "José", "Manoel"]
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUsuarios([...usuarios, "Ismael"])
+    }, 3000)
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello DIO!</h1>
+      {usuarios.map((item) => (
+        <p>{item}</p>
+      ))}
+    </div>
+  );
 }
